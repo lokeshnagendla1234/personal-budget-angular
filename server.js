07b1af811511a014628f1278b3dc24d6640f1953
lokeshnagendla1234 <lnagendl@charlotte.edu>
2024-09-15 19:08:34 -0400

    Added server, Axios, ChartJS and JSON file also D3js chart

--- server.js
+++ server.js
@@ -1,33 +1,27 @@
 const express = require('express');
 const app = express();
+const fs = require('fs');
+const path = require('path');
 const port = 3000;
 
-app.use('/', express.static('public'));
-
-const budget = {
-    myBudget: [
-    {
-      title: 'Eat out',
-      budget:30
-    },
-    {
-      title: 'Rent',
-      budget: 35
-    },
-    {
-      title: 'Groceries',
-      budget: 90
-    },  
- ]
-};      
-app.get('/hello', (req,res) => {
-    res.send('Hello world!');
-});
+app.use('/',express.static('public'));
 
 app.get('/budget', (req, res) => {
-    res.json(budget);
+
+    fs.readFile('./mybudget.json', 'utf8', (err, data) => {
+        if (err) {
+            console.error('Error reading mybudget.json:', err);
+            return res.status(404).send('Budget file are not found');
+        }
+        const mybudget = JSON.parse(data);
+        res.json(mybudget);
+    });
 });
 
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
-    });    
+app.get('/hello', (req,res) => {
+    res.send('Hello World!');
+});
+
+app.listen(port,()=>{
+    console.log(`Server is currently running on port ${port}`);
+});
